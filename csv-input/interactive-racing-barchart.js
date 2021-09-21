e346be42dd1c8b39262cf669e66c5ccf252ddd53
imre.lindi <imre.lindi@codecool.com>
2021-09-21 20:07:24 +0200

    test-csv-input

---
+++ csv-input/interactive-racing-barchart.js
@@ -0,0 +1,42 @@
+import Vizzu from 'https://vizzuhq.github.io/vizzu-beta-release/0.2.0/vizzu.js';
+import dataReady from './csv-input.js'
+
+let chart = new Vizzu('myVizzu');
+
+let vizzuFinished = Promise.all([dataReady, chart.initializing])
+.then(([data, chart]) => 
+{
+	let year = 2017;
+
+	let slider = document.getElementById("year-slider");
+
+	slider.oninput = e =>
+	{
+		year = e.target.value;
+		vizzuFinished = vizzuFinished.then(chart =>
+			chart.animate(
+				{
+					data: { filter: record => record.Year == year },
+					descriptor: { title: year } 
+				},
+				{ duration: '500ms'})
+		);
+	};
+
+	data.filter = record => record.Year == '2017';
+
+	return chart.animate(
+	{
+		data,
+		descriptor: {
+			channels: {
+				x: {attach: ['Country Name'], range: '0,10,1'},
+				y: {attach: ['Count'], range: '0,1500000000,1'},
+				color: {attach: ['Country Name']},
+			},
+			sort: 'experimental',
+			reverse: true,
+			legend: null
+		}
+	});
+})
